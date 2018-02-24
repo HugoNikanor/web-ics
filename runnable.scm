3b5878ba39210939e675a910ff870eefbbab0f27
Hugo Hörnquist <hugo@hornquist.se>
2018-02-24 14:04:00 +0100

    runnable now contains a web server

--- runnable.scm
+++ runnable.scm
@@ -15,14 +15,33 @@
 
 (use-modules (main)
              (sxml simple)
-             (output line))
+
+             (output line)
+
+             (web server)
+             (web request)
+             (web response)
+             (web uri)
+
+             (ice-9 rdelim)
+             )
+
+
+;;; This handler really is horible!
+(define (handler request request- body)
+  ;; split-and-decode-uri-path
+  (let ((path (uri-path (request-uri request))))
+    (displayln path)
+    (cond
+      ((equal? path "/style.css")
+       (values '((content-type text/css))
+               (call-with-input-file "./front/style.css" read-string)))
+      (else
+       (values '((content-type text/html))
+               (with-output-to-string
+                 (lambda ()
+                   (displayln "<!doctype html>")
+                   (sxml->xml (get-sxml-doc *group-evs*)))))))))
 
 (define (main args)
-  (with-output-to-file
-      (string-append
-       (dirname (current-filename))
-       "/front/generated.html")
-    (lambda ()
-      (displayln "<!doctype html>")
-      (sxml->xml (get-sxml-doc *group-evs*)))))
-
+  (run-server handler))
