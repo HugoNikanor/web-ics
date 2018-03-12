34d93e552656a6063a2402bade21f468a4cd9a01
Hugo Hörnquist <hugo@hornquist.se>
2018-03-12 14:09:43 +0100

    Moved server handling code into own file

--- runnable.scm
+++ runnable.scm
@@ -2,85 +2,14 @@
 -e main -s
 !#
 
-;;; I know that main probably should be the main file,
-;;; and that the code file should probably have some
-;;; other name.
-;;; But I will fix that when I break main.scm up into
-;;; different modules.
+(let ((here (dirname (current-filename)))
+      (home (getenv "HOME")))
+  (add-to-load-path (string-append here "/lib/guile-mime/"))
+  (add-to-load-path (string-append home "/lib/guile/"))
+  (add-to-load-path here))
 
-(add-to-load-path (string-append (dirname (current-filename))
-                                 "/lib/guile-mime/"))
-(add-to-load-path (string-append (getenv "HOME")
-                                 "/lib/guile/"))
-(add-to-load-path (dirname (current-filename)))
-
-(use-modules (main)
-             (sxml simple)
-
-             (output line)
-             (css)
-
-             (web server)
-             (web request)
-             (web response)
-             (web uri)
-             (web response)
-
-             (ice-9 rdelim)
-
-             (util)
-             (mime))
-
-(define (get-document)
-  (with-output-to-string
-    (lambda ()
-      (displayln "<!doctype html>")
-      (sxml->xml (get-sxml-doc *sorted-groups*)))))
-
-(define mime (get-mime-hash-table))
-
-(define (404-page)
-  (values (build-response
-           #:code 404
-           #:headers '((content-type text/html)))
-   (with-output-to-string
-     (lambda ()
-       (displayln "<!doctype html>")
-       (sxml->xml
-        `(html (meta (@ (charset "utf-8")))
-               (title "Missing File")
-               (style "
-body {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-
-	margin: 0px;
-	width: 100vw;
-	height: 100vh;
-}")
-
-               "This page unintentionally left blank."))))))
-
-(define *default-mime* "text/plain")
-
-(define (handler request body)
-  (let ((path (split-and-decode-uri-path (uri-path (request-uri request)))))
-    (cond ((null? path)    ; This can't be the best way to check for root
-           (values '((content-type text/html))
-                   (get-document)))
-          ((equal? "file" (car path))
-           (let ((local-path (path-join (cons "./front" (cdr path)))))
-             (if (access? local-path R_OK)
-                 (let* ((extension (file-extension local-path))
-                        (mime-type (or (hash-ref mime extension)
-                                       *default-mime*)))
-                   (values `((content-type ,(string->symbol mime-type)))
-                           (call-with-input-file local-path read-string)))
-                 (404-page))))
-          (else (404-page)))))
+(use-modules (web server)
+             (server-handler))
 
 (define (main args)
   (run-server handler))
-
-
---
+++ server-handler.scm
@@ -0,0 +1,73 @@
+(define-module (server-handler)
+  #:use-module (main)
+  #:use-module (sxml simple)
+
+  #:use-module (output line)
+  #:use-module (css)
+
+  ;; #:use-module (web server)
+  #:use-module (web request)
+  #:use-module (web response)
+  #:use-module (web uri)
+  #:use-module (web response)
+
+  #:use-module (ice-9 rdelim)
+
+  #:use-module (util)
+  #:use-module (mime)
+  #:export (*default-mime* handler))
+
+(define mime (get-mime-hash-table))
+
+(define *default-mime* "text/plain")
+
+(define (handler request body)
+  (let ((path (split-and-decode-uri-path (uri-path (request-uri request)))))
+    (cond ((null? path)    ; This can't be the best way to check for root
+           (values '((content-type text/html))
+                   (get-document)))
+          ((equal? "file" (car path))
+           (let ((local-path (path-join (cons "./front" (cdr path)))))
+             (if (access? local-path R_OK)
+                 (let* ((extension (file-extension local-path))
+                        (mime-type (or (hash-ref mime extension)
+                                       *default-mime*)))
+                   (values `((content-type ,(string->symbol mime-type)))
+                           (call-with-input-file local-path read-string)))
+                 (404-page))))
+          (else (404-page)))))
+
+;; Returns the actual calendar page, as html
+(define (get-document)
+  (with-output-to-string
+    (lambda ()
+      (displayln "<!doctype html>")
+      (sxml->xml (get-sxml-doc *sorted-groups*)))))
+
+;;; Returns the full 404 page, with
+;;; mulitple return values and all!
+(define (404-page)
+  (values (build-response
+           #:code 404
+           #:headers '((content-type text/html)))
+   (with-output-to-string
+     (lambda ()
+       (displayln "<!doctype html>")
+       (sxml->xml
+        `(html (meta (@ (charset "utf-8")))
+               (title "Missing File")
+               (style ,default-style)
+               "404"
+               (br)
+               "This page unintentionally left blank."))))))
+
+(define default-style "
+body {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	margin: 0px;
+	width: 100vw;
+	height: 100vh;
+}")
